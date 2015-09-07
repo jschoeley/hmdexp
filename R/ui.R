e14358b82bb6b97160abb822ee1f1373406096d8
Jonas Schöley <j.schoeley@gmail.com>
2015-09-07 15:09:40 +0200

    Externalize UI Blocks
    Now I got the problem that the timebase radio button appears unchecked. This is probably because I initialize it 2 times. Got to find a solution for that.

--- R/ui.R
+++ R/ui.R
@@ -6,13 +6,57 @@ source("./init.R")
 load("../priv/data/hmdcbook.Rdata")
 cntry_code  <- hmdcbook$Code
 
+# UI Blocks ---------------------------------------------------------------
+
+# country dropdown menu
+cnty_dropdown <-
+  column(4,
+         selectInput("country",
+                     label    = "Choose Country",
+                     choices  = cntry_code, multiple = FALSE,
+                     selected = "SWE")
+  )
+
+# timebase radio button
+timebase_radio <-
+  column(2,
+         radioButtons(inputId = "timebase",
+                      label   = "Choose Timebase",
+                      choices = list(Period  = "period",
+                                     Cohort  = "cohort"),
+                      selected = "period")
+  )
+
+# sex radio button
+sex_radio <-
+  column(2,
+         # sex checkbox
+         radioButtons(inputId = "sex",
+                      label   = "Choose Sex",
+                      choices = list(Total  = "fm",
+                                     Female = "f",
+                                     Male   = "m"),
+                      selected = "fm")
+  )
+
+# about
+about <-
+  column(4,
+         p("Idea & Realization: ",
+           a(href = "https://github.com/jschoeley", "Jonas Schöley")),
+         p("Data Source: ",
+           a(href = "http://www.mortality.org/", "Human Mortality Database"))
+  )
+
+
+
 # Page Layout -------------------------------------------------------------
 
 shinyUI(
   navbarPage(title = "Human Mortality Database Explorer", theme = "bootstrap.css",
-             tabPanel("Mortality Rates",
 
-                      # Mx Pannel -----------------------------------------
+             # Mortality Rates --------------------------------------------
+             tabPanel("Mortality Rates",
 
                       fluidRow(
                         column(12, h4(textOutput("plot_title"), align = "center"))
@@ -21,42 +65,20 @@ shinyUI(
                         column(12, plotOutput("plot"))
                       ),
 
-                      hr()
+                      hr(),
+
+                      fluidRow(cnty_dropdown, timebase_radio, sex_radio, about)
 
              ),
+             # Sex Differences --------------------------------------------
+             tabPanel("Mortality Sex Differences",
 
-  # Bottom Panel ----------------------------------------------------------
+                      hr(),
 
-  fluidRow(
-    column(4,
-           # country dropdown
-           selectInput("country",
-                       label    = "Choose Country",
-                       choices  = cntry_code, multiple = FALSE,
-                       selected = "SWE")
-    ),
-    column(2,
-           # timebase checkbox
-           radioButtons(inputId = "timebase",
-                        label   = "Choose Timebase",
-                        choices = list(Period  = "period",
-                                       Cohort  = "cohort"),
-                        selected = "period")
-    ),
-    column(2,
-           # sex checkbox
-           radioButtons(inputId = "sex",
-                        label   = "Choose Sex",
-                        choices = list(Total  = "fm",
-                                       Female = "f",
-                                       Male   = "m"),
-                        selected = "fm")
-    ),
-    column(4,
-           p("Idea & Realization: ",
-             a(href = "https://github.com/jschoeley", "Jonas Schöley")),
-           p("Data Source: ",
-             a(href = "http://www.mortality.org/", "Human Mortality Database"))
-    )
+                      fluidRow(cnty_dropdown, timebase_radio, about)
+
+                      )
+
+
   )
-))
+)
