de7cf04405cf1b731a5c5f3fa213aa558de7f919
Jonas Schöley <j.schoeley@gmail.com>
2015-09-23 18:07:42 +0200

    Use HMD mx Data; Start Extended Abstract

--- data_download_and_preparation.R
+++ data_download_and_preparation.R
@@ -1,63 +1,39 @@
-# Init --------------------------------------------------------------------
-
 library(hmdget) # download human mortality database data (https://github.com/jschoeley/hmdget)
 library(dplyr)  # data verbs, operations on subsets of data
 library(tidyr)  # tidy data, convert between long and wide
-library(readr)  # fast csv read
+library(readr)  # fast csv read and write
+
+# HMD credentials
+username <- "***"
+password <- "***"
 
 # Download HMD data -------------------------------------------------------
 
-# HMD country codes
-cntry <- hmdcbook$Code
+hmd_mx <- HMDget(.country = hmdcbook$Code,
+                 .timeframe = "p+c", .measure = "mx",
+                 username, password)
 
-# period and cohort death counts across countries
-death <- HMDget(.country = cntry, .timeframe = "p+c", .measure = "Dx",
-                username, password)
+# hmd_mx <- read_csv("./priv/hmdmx.csv")
 
-# period and cohort exposures across countries
-exp <- HMDget(.country = cntry, .timeframe = "p+c", .measure = "Nx",
-              username, password)
+# Transform Data ----------------------------------------------------------
 
-# join Dx and Nx
-hmdDxNx <- full_join(death, exp)
-
-# write mortality data to csv
-write_csv(hmdDxNx, path = "./priv/misc/hmdDxNx.csv")
-
-# Transform ---------------------------------------------------------------
-
-# read downloaded data
-read.csv("./priv/hmdDxNx.csv",
-         stringsAsFactors = FALSE,
-         na.strings = "NA") -> hmd
-
-hmd %>%
+hmd_mx %>%
   # variable names to lowercase
   rename(country = Country, timebase = Timeframe, sex = Sex,
-         year = Year, age = Age, dx = Dx, nx = Nx) %>%
-  # rename variable levels
+         year = Year, age = Age) %>%
   mutate(
+    # rename variable levels
     timebase = tolower(timebase),
-    sex = ifelse(sex == "Female", "f", ifelse(sex == "Male", "m", "fm"))
-  ) %>%
-  # remove rows with 0 exposure
-  filter(nx != 0) %>%
-  mutate(
-    # compute rounded mortality rates
-    mx = round(dx / nx, digits = 5),
-    # replace 0 mortality rate with NA
-    mx = ifelse(mx == 0, NA, mx),
+    sex      = ifelse(sex == "Female", "f", ifelse(sex == "Male", "m", "fm")),
     # project cohort-age data onto period-age grid
-    year = ifelse(timebase == "cohort", year + age, year)
+    year     = ifelse(timebase == "cohort", year + age, year),
+    # set mx of 0 to NA
+    mx       = ifelse(mx == 0, NA, mx)
   ) %>%
-  # exclude raw death counts and exposures
-  select(-dx, -nx) %>%
   # remove NA's
-  na.omit() %>%
-  # convert to dplyr data frame
-  as_data_frame() -> hmd_mx
+  na.omit() -> hmd_mx
 
-save(hmd_mx, file = "./priv/data/hmd_mx.Rdata")
+save(hmd_mx, file = "./R/data/hmd_mx.Rdata")
 
 # derive data set for mortality rate sex differences
 hmd_mx %>%
@@ -68,4 +44,4 @@ hmd_mx %>%
   # strip sex specific mortality rates
   select(-f, -m) -> hmd_mx_sex_diff
 
-save(hmd_mx_sex_diff, file = "./priv/data/hmd_mx_sex_diff.Rdata")
+save(hmd_mx_sex_diff, file = "./R/data/hmd_mx_sex_diff.Rdata")
