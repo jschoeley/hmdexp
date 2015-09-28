4cff641cd37ef3bb60454db8b1668f49f1704586
Jonas Schöley <j.schoeley@gmail.com>
2015-09-28 14:03:02 +0200

    Externalize UI Elements

--- R/ui.R
+++ R/ui.R
@@ -1,83 +1,7 @@
 # Input -------------------------------------------------------------------
 
 source("./init.R")
-
-# list of countries in data
-load("./data/hmdcbook.Rdata")
-#cntry_code  <- hmdcbook$Code
-cntry_lab  <- hmdcbook$Label
-
-# UI Blocks ---------------------------------------------------------------
-
-# country dropdown menu
-cntry_dropdown <-
-  column(4,
-         selectInput("country",
-                     label     = "Choose Country",
-                     choices   = cntry_lab, multiple = FALSE,
-                     selected  = "Sweden",
-                     selectize = FALSE)
-  )
-
-# country comparison dropdown menu
-cntry1_diff_dropdown <-
-  column(2,
-         selectInput("country_1",
-                     label     = "Choose Country 1",
-                     choices   = cntry_lab, multiple = FALSE,
-                     selected  = "Sweden",
-                     selectize = FALSE)
-  )
-cntry2_diff_dropdown <-
-  column(2,
-         selectInput("country_2",
-                     label     = "Choose Country 2",
-                     choices   = cntry_lab, multiple = FALSE,
-                     selected  = "Denmark",
-                     selectize = FALSE)
-  )
-
-# timebase radio button
-timebase_radio <-
-  column(1,
-         radioButtons(inputId = "timebase",
-                      label   = "Choose Timebase",
-                      choices = list(Period  = "period",
-                                     Cohort  = "cohort"),
-                      selected = "period")
-  )
-
-# sex radio button
-sex_radio <-
-  column(1,
-         # sex checkbox
-         radioButtons(inputId = "sex",
-                      label   = "Choose Sex",
-                      choices = list(Total  = "fm",
-                                     Female = "f",
-                                     Male   = "m"),
-                      selected = "fm")
-  )
-
-# about
-about <-
-  column(6,
-         p("Idea & Realization:", br(),
-           a(href = "https://github.com/jschoeley", "Jonas Schöley"), "at",
-           a(href = "http://www.sdu.dk/en/om_sdu/institutter_centre/maxo", br(),
-             "Max-Planck Odense Center on the Biodemography of Aging")),
-         p("Data Source:", br(),
-           a(href = "http://www.mortality.org/", "Human Mortality Database")),
-         p("A big thanks goes towards the Human Mortality Database team.
-           Their magnificient work makes it possible for laymen, students and
-           researchers alike to explore the incredible rise of human life expectancy,
-           the mysteries of the gender differences in survival or the global
-           variation in mortality patterns. This project would not be possible
-           without free access to the HMD data."),
-          p("If you want to dig into the underlying data yourself just go to",
-           a(href = "http://www.mortality.org/", "http://www.mortality.org/"),
-           "and register.")
-  )
+source("./ui_elements.R")
 
 # Page Layout -------------------------------------------------------------
 
@@ -87,55 +11,42 @@ shinyUI(
              header = tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "style.css")),
 
              # Mortality Rates --------------------------------------------
+
              tabPanel(value = "tab_mx", title = "Mortality Rates",
 
-                      fluidRow(class = "plot_main_title",
-                               h4(textOutput("plot_mx_main_title"),
-                                  align = "center")),
-                      fluidRow(class = "plot_sub_title",
-                               h5(textOutput("plot_mx_sub_title"),
-                                  align = "center")),
+                      UIPlotTitle("plot_mx_main_title"),
+                      UIPlotSubTitle("plot_mx_sub_title"),
 
-                      fluidRow(class = "plotRow",
-                               plotOutput("plot_mx", height = "100%"))
+                      UIPlot("plot_mx")
 
              ),
 
              # Sex Differences --------------------------------------------
+
              tabPanel(value = "tab_mx_sex_diff", title = "Sex Differences",
 
-                      fluidRow(class = "plot_main_title",
-                               h4(textOutput("plot_mx_sex_diff_main_title"),
-                                  align = "center")),
+                      UIPlotTitle("plot_mx_sex_diff_main_title"),
+                      UIPlotSubTitle("plot_mx_sex_diff_sub_title"),
 
-                      fluidRow(class = "plot_sub_title",
-                               h5(textOutput("plot_mx_sex_diff_sub_title"),
-                                  align = "center")),
-
-                      fluidRow(class = "plotRow",
-                               plotOutput("plot_mx_sex_diff", height = "100%"))
+                      UIPlot("plot_mx_sex_diff")
 
              ),
 
              # Country Differences ----------------------------------------
+
              tabPanel(value = "tab_mx_cntry_diff", title = "Country Comparison",
 
-                      fluidRow(class = "plot_main_title",
-                               h4(textOutput("plot_mx_country_diff_main_title"),
-                                  align = "center")),
+                      UIPlotTitle("plot_mx_country_diff_main_title"),
+                      UIPlotSubTitle("plot_mx_country_diff_sub_title"),
 
-                      fluidRow(class = "plot_sub_title",
-                               h5(textOutput("plot_mx_country_diff_sub_title"),
-                                  align = "center")),
-
-                      fluidRow(class = "plotRow",
-                               plotOutput("plot_mx_cntry_diff", height = "100%"))
+                      UIPlot("plot_mx_cntry_diff")
 
              ),
 
              hr(),
 
              # Settings Panel ---------------------------------------------
+
              fluidRow(
                conditionalPanel("(input.navbar == 'tab_mx' || input.navbar == 'tab_mx_sex_diff')",
                                 cntry_dropdown),
---
+++ R/ui_elements.R
@@ -0,0 +1,97 @@
+# Input -------------------------------------------------------------------
+
+# list of countries in data
+load("./data/hmdcbook.Rdata")
+# country labels
+cntry_lab  <- hmdcbook$Label
+
+# UI Elements -------------------------------------------------------------
+
+# country dropdown menu
+cntry_dropdown <-
+  column(4,
+         selectInput("country",
+                     label     = "Choose Country",
+                     choices   = cntry_lab, multiple = FALSE,
+                     selected  = "Sweden",
+                     selectize = FALSE)
+  )
+
+# country comparison dropdown menu
+cntry1_diff_dropdown <-
+  column(2,
+         selectInput("country_1",
+                     label     = "Choose Country 1",
+                     choices   = cntry_lab, multiple = FALSE,
+                     selected  = "Sweden",
+                     selectize = FALSE)
+  )
+cntry2_diff_dropdown <-
+  column(2,
+         selectInput("country_2",
+                     label     = "Choose Country 2",
+                     choices   = cntry_lab, multiple = FALSE,
+                     selected  = "Denmark",
+                     selectize = FALSE)
+  )
+
+# timebase radio button
+timebase_radio <-
+  column(1,
+         radioButtons(inputId = "timebase",
+                      label   = "Choose Timebase",
+                      choices = list(Period  = "period",
+                                     Cohort  = "cohort"),
+                      selected = "period")
+  )
+
+# sex radio button
+sex_radio <-
+  column(1,
+         # sex checkbox
+         radioButtons(inputId = "sex",
+                      label   = "Choose Sex",
+                      choices = list(Total  = "fm",
+                                     Female = "f",
+                                     Male   = "m"),
+                      selected = "fm")
+  )
+
+# about
+about <-
+  column(6,
+         p("Idea & Realization:", br(),
+           a(href = "https://github.com/jschoeley", "Jonas Schöley"), "at",
+           a(href = "http://www.sdu.dk/en/om_sdu/institutter_centre/maxo", br(),
+             "Max-Planck Odense Center on the Biodemography of Aging")),
+         p("Data Source:", br(),
+           a(href = "http://www.mortality.org/", "Human Mortality Database")),
+         p("A big thanks goes towards the Human Mortality Database team.
+           Their magnificient work makes it possible for laymen, students and
+           researchers alike to explore the incredible rise of human life expectancy,
+           the mysteries of the gender differences in survival or the global
+           variation in mortality patterns. This project would not be possible
+           without free access to the HMD data."),
+         p("If you want to dig into the underlying data yourself just go to",
+           a(href = "http://www.mortality.org/", "http://www.mortality.org/"),
+           "and register.")
+  )
+
+# UI Functions ------------------------------------------------------------
+
+UIPlotTitle <- function(title) {
+  fluidRow(class = "plot_main_title",
+           h4(textOutput(title),
+              align = "center"))
+}
+
+UIPlotSubTitle <- function(subtitle) {
+  fluidRow(class = "plot_sub_title",
+           h5(textOutput(subtitle),
+              align = "center"))
+}
+
+UIPlot <- function (plot) {
+  fluidRow(class = "plotRow",
+           plotOutput(plot, height = "100%"))
+}
