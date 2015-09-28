8d082949a4b9c04a189a874dd6aa8abe1463df9b
Jonas Schöley <j.schoeley@gmail.com>
2015-09-28 15:41:14 +0200

    Split Functions into Lexis-Grid and Heatmap Parts

--- R/fnct-plot.R
+++ R/fnct-plot.R
@@ -33,30 +33,14 @@ ybreak <- seq(0, 110, 10)
 # age limits
 ylimit <- c(0, 112)
 
-# Plot mx -----------------------------------------------------------------
 
-#' Plot mx Values
-PlotMx <- function (x) {
+# Lexis Grid Base Plot ----------------------------------------------------
 
-  # colour scale title
-  label <- expression(atop(atop("Deaths per",
-                                "10,000 Person Years"),
-                           m(x)%*%10000))
-
-  # plot
-  plot_mx <-
-    ggplot(x, aes(x = year, y = age)) +
+PlotLexisGrid <- function (x) {
+  ggplot(x, aes(x = year, y = age)) +
     # cohort lines
     geom_abline(intercept = seq(-2000, 2000, 10),
                 colour = "grey50", size = 0.25, lty = 2) +
-    # heatmap
-    geom_tile(aes(fill = mx)) +
-    # discrete colour scale
-    scale_fill_brewer(label,
-                      palette = "PuBuGn",
-                      # plot the full scale even if not all colours are used
-                      drop = FALSE) +
-    guides(fill = guide_legend(reverse = TRUE)) +
     # custom xy scale labels
     scale_x_continuous("Year", limits = xlimit,
                        breaks = xbreak, labels = xlabel,
@@ -66,7 +50,32 @@ PlotMx <- function (x) {
                        expand = c(0, 0.5)) +
     # equidistant xy-coordinates
     coord_equal() +
+    # add theme
     theme_hmdexp
+}
+
+
+# Plot mx -----------------------------------------------------------------
+
+#' Plot mx Values
+PlotMx <- function (x) {
+
+  # title for colour scale legend
+  fill_label <- expression(atop(atop("Deaths per",
+                                     "10,000 Person Years"),
+                                m(x)%*%10000))
+
+  # plot
+  plot_mx <-
+    PlotLexisGrid(x) +
+    # heatmap
+    geom_tile(aes(fill = mx)) +
+    # discrete colour scale
+    scale_fill_brewer(fill_label,
+                      palette = "PuBuGn",
+                      # plot the full scale even if not all colours are used
+                      drop = FALSE) +
+    guides(fill = guide_legend(reverse = TRUE))
 
   return(plot_mx)
 
@@ -77,32 +86,22 @@ PlotMx <- function (x) {
 #' Plot mx Sex Differences
 PlotMxSexDiff <- function (x) {
 
+  # title for colour scale legend
+  fill_label <- expression(atop(atop("Absolute Difference in",
+                                     "Female and Male Mortality Rates"),
+                                m(x)[F]-m(x)[M]))
+
   # plot
   plot_mx_sex_diff <-
-    ggplot(x, aes(x = year, y = age)) +
-    # cohort lines
-    geom_abline(intercept = seq(-2000, 2000, 10),
-                colour = "grey50", size = 0.25, lty = 2) +
+    PlotLexisGrid(x) +
     # heatmap
     geom_tile(aes(fill = mx_sex_diff)) +
     # divergent colour scale
-    scale_fill_manual(expression(atop(atop("Absolute Difference in",
-                                           "Female and Male Mortality Rates"),
-                                      m(x)[F]-m(x)[M])),
+    scale_fill_manual(fill_label,
                       values = rev(brewer.pal(10, "RdBu")),
                       # plot the full scale even if not all colours are used
                       drop = FALSE) +
-    guides(fill = guide_legend(reverse = TRUE)) +
-    # custom xy scale labels
-    scale_x_continuous("Year", limits = xlimit,
-                       breaks = xbreak, labels = xlabel,
-                       expand = c(0, 0.5)) +
-    scale_y_continuous("Age", limits = ylimit,
-                       breaks = ybreak,
-                       expand = c(0, 0.5)) +
-    # equidistant xy-coordinates
-    coord_equal() +
-    theme_hmdexp
+    guides(fill = guide_legend(reverse = TRUE))
 
   return(plot_mx_sex_diff)
 
@@ -113,32 +112,22 @@ PlotMxSexDiff <- function (x) {
 #' Plot mx Country Differences
 PlotMxCntryDiff <- function (x) {
 
+  # title for colour scale legend
+  fill_label <- expression(atop(atop("Absolute Difference in",
+                                     "Country 1 and Country 2 Mortality Rates"),
+                                m(x)[1]-m(x)[2]))
+
   # plot
   plot_mx_cntry_diff <-
-    ggplot(x, aes(x = year, y = age)) +
-    # cohort lines
-    geom_abline(intercept = seq(-2000, 2000, 10),
-                colour = "grey50", size = 0.25, lty = 2) +
+    PlotLexisGrid(x) +
     # heatmap
     geom_tile(aes(fill = mx_country_diff)) +
-    # divergent, cntn. colour scale
-    scale_fill_brewer(expression(atop(atop("Absolute Difference in",
-                                           "Country 1 and Country 2 Mortality Rates"),
-                                      m(x)[1]-m(x)[2])),
+    # divergent colour scale
+    scale_fill_brewer(fill_label,
                       palette = "BrBG",
                       # plot the full scale even if not all colours are used
                       drop = FALSE) +
-    guides(fill = guide_legend(reverse = TRUE)) +
-    # custom xy scale labels
-    scale_x_continuous("Year", limits = xlimit,
-                       breaks = xbreak, labels = xlabel,
-                       expand = c(0, 0.5)) +
-    scale_y_continuous("Age", limits = ylimit,
-                       breaks = ybreak,
-                       expand = c(0, 0.5)) +
-    # equidistant xy-coordinates
-    coord_equal() +
-    theme_hmdexp
+    guides(fill = guide_legend(reverse = TRUE))
 
   return(plot_mx_cntry_diff)
 
